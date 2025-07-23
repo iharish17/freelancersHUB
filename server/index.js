f6269b6c12f920a388bc9ebe7542d812f96ce9a2
iharish17 <241448@kit.ac.in>
2025-07-23 18:16:48 +0530

    Initial commit

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "https://freelancers-hub.vercel.app", credentials: true }));
 app.use(express.json());
 
 // Routes
