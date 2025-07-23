8b630ec47e834392ede39168d7e3fbbaeb712a9b
iharish17 <241448@kit.ac.in>
2025-07-23 18:12:14 +0530

    Initial commit

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://freelancershub.onrender.com",
+  baseURL: process.env.REACT_APP_API_URL,
   withCredentials: true,
 });
 
